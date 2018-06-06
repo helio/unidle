b2c303f070d3854a52fa63e21844bd1437076a71
Rui Sereno <ruisereno@significa.pt>
2018-06-06 14:13:57 +0100

    hero, vision and calc styles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,23 @@
 // This is where it all goes :)
+
+var rangeEl = document.getElementsByClassName('js-range-item');
+
+function updateRangeBar(e) {
+  var min = e.target.min,
+    max = e.target.max,
+    val = e.target.value;
+
+  e.target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+}
+
+function updateRangeValue(element, value) {
+  var target = element.parentNode.getElementsByClassName('js-range-value')[0];
+  target.innerHTML = value + '%';
+}
+
+for (var i = 0; i < rangeEl.length; i++) {
+  rangeEl[i].addEventListener('input', function(e) {
+    updateRangeBar(e);
+    updateRangeValue(this, this.value);
+  });
+}
