a4ccb39c62c8e9e6346ba5758afa0cf4cd237beb
Kevin Häfeli <khaefeli@users.noreply.github.com>
2018-08-06 17:06:43 +0200

    Update config.rb

--- config.rb
+++ config.rb
@@ -40,7 +40,11 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+# minify css / js
+configure :build do
+   activate :minify_css
+   activate :minify_javascript
+end
+
+# compress impages
+activate :imageoptim
